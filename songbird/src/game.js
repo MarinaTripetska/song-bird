26896d9589317b664dac7a13aa017edeaa634c62
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-11-19 11:39:47 +0100

    feat: add show img on correct answer

--- songbird/src/game.js
+++ songbird/src/game.js
@@ -5,15 +5,18 @@ import createQuestionPlayer from "./js/createQuestionPlayer";
 import birdsInCategory from "./js/birdsInCategory";
 import createAnswerDesc from "./js/createAnswerDesc";
 
+const audioPlayerEl = document.querySelector(".quiz__audio-player");
+const optionsEl = document.querySelectorAll("[name='option']");
+const descThumb = document.querySelector(".answer__description.description");
+const birdNameEl = document.querySelector(".quiz__question-name");
+const birdImgEl = document.querySelector(".quiz__img");
+
 let categoryCount = 0;
 let score = 0;
 
 window.addEventListener("load", () => showNewQuestion(categoryCount, dataBase));
 
 function showNewQuestion(categoryCount, dataBase) {
-  const audioPlayerEl = document.querySelector(".quiz__audio-player");
-  const optionsEl = document.querySelectorAll("[name='option']");
-
   const incognitoBird = randomBirdInCategory(categoryCount, dataBase);
   const birds = birdsInCategory(categoryCount, dataBase);
   createQuestionPlayer(audioPlayerEl, incognitoBird.audio, "audio-player");
@@ -21,16 +24,19 @@ function showNewQuestion(categoryCount, dataBase) {
   optionsEl.forEach((el, i) => {
     el.setAttribute("value", birds[i].name);
     el.nextElementSibling.append(birds[i].name);
-    el.addEventListener("click", (e) => chooseBird(e, incognitoBird, birds));
+    el.addEventListener("click", (e) =>
+      chooseBird(e, incognitoBird, birds, el)
+    );
   });
 }
 
 function chooseBird(e, incognitoBird, birds) {
+  // let scoreInSection = birds.length;
+  // console.log(scoreInSection);
   const chooseBirdName = e.target.value;
   const chooseBird = birds.find((bird) => bird.name === chooseBirdName);
 
   const htmlTemplate = createAnswerDesc(chooseBird);
-  const descThumb = document.querySelector(".answer__description.description");
   descThumb.innerHTML = htmlTemplate;
 
   const smallAudioEl = descThumb.querySelector(".answer-audio");
@@ -44,11 +50,14 @@ function chooseBird(e, incognitoBird, birds) {
   if (incognitoBird.name === chooseBirdName) {
     const winAudio = new Audio("./assets/audio/smart.mp3");
     winAudio.play();
-    // TODO:  show name on top, add score, add sound, add animation(green color)
+    birdNameEl.textContent = chooseBirdName;
+    birdImgEl.src = chooseBird.image;
+
+    // TODO: add score, add animation(green color)
   } else {
     const winAudio = new Audio("./assets/audio/click.mp3");
     winAudio.play();
-    // TODO: add sound, add animation (red color)
+    // TODO: , add animation (red color)
   }
 }
 
