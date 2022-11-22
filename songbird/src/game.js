c430e1c7e118ed9f7a178e5c031a3b9fd3354fdd
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-11-22 01:26:03 +0100

    feat: add style logic for current category

--- songbird/src/game.js
+++ songbird/src/game.js
@@ -121,4 +121,20 @@ function goToNextLevel() {
   ++categoryCount;
 
   showNewQuestion();
+  activateNextCategory();
+}
+
+function activateNextCategory() {
+  const categoriesList = document.querySelectorAll("[data-active]");
+  let index = 0;
+  categoriesList.forEach((el, i) => {
+    if (el.previousElementSibling) {
+      if (el.previousElementSibling.dataset.active === "true") {
+        el.previousElementSibling.dataset.active = "false";
+        index = i;
+      }
+    }
+  });
+
+  categoriesList[index].dataset.active = "true";
 }
